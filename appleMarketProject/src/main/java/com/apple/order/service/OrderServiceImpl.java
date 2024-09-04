07d412497a636b117d5cdc01f885522f6e96bc5f
LEEJAHEUN <58925046+LEEJAHEUN@users.noreply.github.com>
2024-09-04 12:04:08 +0900

    불필요한 어노테이션 삭제

--- appleMarketProject/src/main/java/com/apple/order/service/OrderServiceImpl.java
+++ appleMarketProject/src/main/java/com/apple/order/service/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.apple.order.domain.Order;
 import com.apple.order.repository.OrderRepository;
