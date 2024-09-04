5e1573cdd24fc60bf1b615655b232373397a1dcc
jiwoon02 <yjiwoon0217@gmail.com>
2024-09-04 10:36:13 +0900

    구매내역 구현중..

--- appleMarketProject/src/main/java/com/apple/config/SecurityConfig.java
+++ appleMarketProject/src/main/java/com/apple/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig{
 		http
         	.authorizeHttpRequests((auth) -> auth
 
-        			.requestMatchers("/product/insertForm","/product/updateForm", "/user/locationForm").hasAuthority("USER")
+        			.requestMatchers("/product/insertForm","/product/updateForm", "/user/locationForm","/mypage/**").hasAuthority("USER")
         			.requestMatchers("/user/loginForm").not().hasAuthority("USER")
         			.anyRequest().permitAll());
 		//JWTFilter 등록
