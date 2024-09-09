62415ead30d7593f46e00403a2e3276c7215473c
LEEJAHEUN <58925046+LEEJAHEUN@users.noreply.github.com>
2024-09-09 15:26:24 +0900

    Merge branch 'feature/leejaheun' -------------------------------------------- 되돌리기 전 엉망진창 커밋
    # Conflicts:
    #	appleMarketProject/src/main/java/com/apple/order/controller/OrderController.java
    #	appleMarketProject/src/main/java/com/apple/product/service/ProductService.java
    #	appleMarketProject/src/main/java/com/apple/product/service/ProductServiceImpl.java
    #	appleMarketProject/src/main/java/com/apple/user/service/UserService.java
    #	appleMarketProject/src/main/java/com/apple/user/service/UserServiceImpl.java
    #	appleMarketProject/src/main/resources/static/js/product/productDetail.js
    #	appleMarketProject/src/main/resources/templates/product/productDetail.html
    #	appleMarketProject/src/main/resources/templates/template/client/fragments/block.html

--- appleMarketProject/src/main/reactchat/src/App.js
+++ appleMarketProject/src/main/reactchat/src/App.js
@@ -1,23 +1,20 @@
-
-import React, { useEffect, useMemo, useState } from 'react';
-import { SendBirdProvider, TypingIndicatorType } from '@sendbird/uikit-react';
+import React, {useState} from 'react';
+import { SendBirdProvider } from '@sendbird/uikit-react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MyChannelList from './MyChannelList.js';
 import ChannelSelector from './ChannelSelector.js';
-import GroupChannel from '@sendbird/uikit-react/GroupChannel';
+import GroupChannel from '@sendbird/uikit-react/GroupChannel';  // GroupChannel 임포트
 import '@sendbird/uikit-react/dist/index.css';
 import './App.css';
 
 const App = () => {
   const [channelData, setChannelData] = useState({
-    appId: 'YOUR_APP_ID',
-    buyerId: 'YOUR_USER_ID',
-    buyerNickname: 'YOUR_USER_NICKNAME',
+    appId: '',
+    buyerId: '',
+    buyerNickname: '',
   });
-  const [currentChannelUrl, setCurrentChannelUrl] = useState('');
-
+  const [currentChannelUrl, setCurrentChannelUrl] = useState('');  // 채널 URL 상태 관리
   const { appId, buyerId, buyerNickname } = channelData;
-
   const myColorSet = {
     '--sendbird-light-primary-500': '#9D091E',
     '--sendbird-light-primary-400': '#BF0711',
@@ -30,44 +27,33 @@ const App = () => {
     setCurrentChannelUrl(channelUrl);
   };
 
-  const uikitOptions = useMemo(() => ({
-    groupChannel: {
-      enableTypingIndicator: true,
-      typingIndicatorTypes: new Set([TypingIndicatorType.Bubble, TypingIndicatorType.Text]),
-    }
-  }), []);
-
-  useEffect(() => {
-    console.log("uikitOptions 설정값: ", uikitOptions);
-  }, [uikitOptions]);
-
   return (
     <SendBirdProvider
-      appId={appId}
-      userId={buyerId}
-      nickname={buyerNickname}
+      appId={appId} 
+      userId={buyerId} 
+      nickname={buyerNickname}  
       colorSet={myColorSet}
-      uikitOptions={uikitOptions}
     >
       <Router>
         <div className='app-container'>
           <Routes>
-            <Route
-              path="/chatroom/chatroom"
+            <Route 
+              path="/chatroom/chatroom" 
               element={
-                <ChannelSelector
-                  setChannelData={setChannelData}
-                  onChannelSelected={handleChannelSelected}
+                <ChannelSelector 
+                  setChannelData={setChannelData} 
+                  onChannelSelected={handleChannelSelected} 
                 />
-              }
+              } 
             />
-            <Route
-              path="/chatroom/chatroomList"
+            <Route 
+              path="/chatroom/chatroomList" 
               element={
-                <MyChannelList
-                  setChannelData={setChannelData}
+                <MyChannelList 
+                  setChannelData={setChannelData} 
+                  onChannelSelected={handleChannelSelected} 
                 />
-              }
+              } 
             />
           </Routes>
         </div>
